8294288b9b9a043682fbfa9915887282cbe7d94c
vipulsurw <survase.vipul@outlook.com>
2024-03-24 21:31:14 +0000

    wame start

--- WAME-optimizer.py
+++ WAME-optimizer.py
@@ -8,7 +8,7 @@ This project utilizes code from the following sources:
 
 
 https://www.elen.ucl.ac.be/Proceedings/esann/esannpdf/es2017-50.pdf
-  -Please ensure to cite Mosca et al (2017) if any of the code following is used. Available from: 
+  -Please ensure to cite Mosca et al (2017) if any of the code following is used.
 
 """
 
@@ -33,4 +33,25 @@ np.random.seed(404)
 
 X_train = X_train.reshape(60000, 28, 28, 1)
 X_test = X_test.reshape(10000, 28, 28, 1)
+
+# RANDOMLY SORT X_test/y_test
+indexes = np.arange(X_test.shape[0])
+for _ in range(5): indexes = np.random.permutation(indexes)  # shuffle 5 times!
+X_test = X_test[indexes]
+y_test = y_test[indexes]
+
+#Implement WAME optimizer using Keras backend
+
+from keras.optimizers import Optimizer
+from keras import backend as K
+
+if K.backend() == 'tensorflow':
+    import tensorflow as tf
+
+
+class WAME(Optimizer):
+    
+    def __init__(self, learning_rate = 0.0001, alpha=0.9, eta_plus = 1.2, eta_minus = 0.1,
+                 zeta_min = 0.01, zeta_max = 100, epsilon = 1e-12, **kwargs):
+        
 
